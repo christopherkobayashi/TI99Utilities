0de4673e0f5dfab0b1a3599bb56259b28710b041
christopher.kobayashi <software@disavowed.jp>
2016-02-14 17:34:52 +0900

    Add function to build directory entries suitable for sending directly to wire.  Need to skip ".nfs" files.

--- hdx.py
+++ hdx.py
@@ -4,6 +4,119 @@ import time
 from time import sleep
 import serial
 import sys
+import os
+
+fiad_dir = "/home/wileyc/ti/hdx/fiad"
+foad_dir = "/home/wileyc/ti/hdx/fiad"
+reading_fiad = 0
+reading_foad = 0
+
+foad_list = os.listdir(foad_dir)
+fiad_list = os.listdir(fiad_dir)
+fiad_counter = 0
+
+debug = 1
+
+# record 0 (which we return as record +1) contains null filename, "0" as
+# filetype, BIGNUM as blocks used, same BIGNUM as filesize, then 00 and "524288"
+# (prefixed by 0) as timestamp?
+#
+# final record is "000000" for every field, except for "03 07 14" at the end.
+# it's copied from the previous file -- is Fred not initializing?
+
+
+def build_directory_record(file, ti_fd):
+	global fiad_counter
+	buffer = ""
+	fullpath = fiad_dir+"/"+file
+	ti_filename = ""
+	file_blocks_radix = list()
+	file_size_radix = list()
+
+	if os.path.isdir(fullpath):
+	  ti_filename = file
+	  file_size_str = "0"
+	  file_size = 0
+	  file_size_radix.append(chr(0x00))
+	  file_blocks_str = "0"
+	  file_blocks = 0
+	  file_blocks_radix.append(chr(0x00))
+	  
+	elif os.path.isfile(fullpath) and file[0] != '.':
+	  f = open(fullpath, "rb")
+	  ti_filename = f.read(10)
+	  ti_filename = ti_filename.rstrip(' \t\r\n\0')
+	  f.seek(0x0c)
+	  ti_filetype = f.read(1)
+	  ti_filetype = 0x05 # ord ti_filetype
+	  file_size = os.path.getsize(fiad_dir+"/"+file) - 128
+
+	  file_blocks = file_size / 256
+	  file_blocks_str = str(file_blocks)
+	  if len(file_blocks_str) % 2 != 0:
+	 	file_blocks_str = "0"+file_blocks_str
+
+	  if len(file_blocks_str) > 2:
+	    for i in range (0, len(file_blocks_str), 2):
+		c = file_blocks_str[i:i+2]
+		file_blocks_radix.append(chr(int(c)))
+	  else:
+		file_blocks_radix.append(file_blocks_str)
+
+	  file_size_str = str(file_size)
+	  if len(file_size_str) % 2 != 0:
+	 	file_size_str = "0"+file_size_str
+
+	  for i in range (0, len(file_size_str), 2):
+		c = file_size_str[i:i+2]
+		file_size_radix.append(chr(int(c)))
+	else:
+	  print "filename is weird, noop"
+	  return ""
+
+	if debug > 0 and ti_filename:
+	  print ti_filename + " ", os.path.getsize(fiad_dir+"/"+file), "blocks: " + file_blocks_str + " size: " + file_size_str
+	  for i in file_blocks_radix:
+	    print " - blocks radix - " + i.encode("hex")
+	  for i in file_size_radix:
+	    print " - size radix - " + i.encode("hex")
+
+	 # can easily add the timestamp, as documented but not implemented
+	 # by Fred's application.
+
+	 # okay, build buffer
+
+	fiad_counter += 1
+	buffer = chr(0x40) + chr(0x00) + chr(0x30 + ti_fd) + chr(0x00)
+	buffer += chr(fiad_counter)
+	buffer += chr(0x92) # fixed length
+	buffer += chr(len(ti_filename))
+	buffer += ti_filename
+	buffer += chr(0x08) + chr(0x40)
+	buffer += chr(0x05)  # hardcoding to program for now -- fixme!
+	for i in range(0, 7):
+	  buffer += chr(0x00)
+	buffer += chr(0x08) + chr(0x40 + len(file_blocks_str)-1)
+	for i in file_blocks_radix:
+	  buffer += i
+	for i in range(0, 8 - len(file_blocks_radix)):
+	  buffer += chr(0x00)
+	buffer += chr(0x08) + chr(0x40 + len(file_size_str)-1)
+	for i in file_size_radix:
+	  buffer += i
+	for i in range(0, 8 - len(file_size_radix)):
+	  buffer += chr(0x00)
+
+	for i in range (len(buffer), 0x92):
+	  buffer += chr(0x00)
+	if debug > 0:
+
+	  for i in buffer:
+		print "Hex: " + i.encode("hex") + " Asc: " + i
+	  print hex(len(buffer))
+	  print
+	  print
+	return buffer
 
 def serial_write(string):
 	checksum = 0
