3fbb0407eea1afd8875666634a18f1875c4a4e9a
Christopher Kobayashi <wileyc@elan.disavowed.jp>
2016-04-22 10:31:55 +0900

    Refactor, clean up.

--- printer_listener.py
+++ printer_listener.py
@@ -8,6 +8,9 @@ import serial
 import sys
 import os
 
+ser = serial.Serial()
+f = ""
+
 def control_c(signum, frame):
     print "control-c hit, closing up shop"
     if ser:
@@ -16,43 +19,63 @@ def control_c(signum, frame):
       f.close()
     sys.exit(1)
 
-ser = serial.Serial(
-    port='/dev/ttyUSB0',
-    baudrate=38400,
+def usage():
+    print "usage: printer_listener.py -d <serial device> -s <speed>"
+    sys.exit(1)
+
+def main(argv=None):
+    line = ""
+    speed = 0
+    device = ""
+
+    try:
+        opts, args = getopt.getopt(sys.argv[1:], "hd:s:f:", ["help", "device=", "speed=", "filter="]) 
+    except getopt.GetoptError as err:
+	usage()
+    for o, a in opts:
+	if o in ("-d", "--device"):
+	    device = a
+	if o in ("-s", "--speed"):
+	    speed = a
+	if f in ("-f", "--filter):
+	    filter = a
+
+    if speed == 0 or device == "":
+	usage()
+
+    signal.signal(signal.SIGINT, control_c)
+
+    ser.port=device
+    ser.baudrate=speed
     parity=serial.PARITY_NONE,
     stopbits=serial.STOPBITS_ONE,
     bytesize=serial.EIGHTBITS,
     dsrdtr=True,
     timeout=15
-)
 
-line = ""
-f = ""
+    ser.open()
+    ser.flush()
 
-signal.signal(signal.SIGINT, control_c)
+    while 1:
 
-ser.flush()
+        print "printer listener listening"
+        while not line:
+	    sleep(5)
+	    line = ser.read(1)
 
-while 1:
+        print "printer active"
+        now = datetime.datetime.now()
+        nowstring = now.strftime("%Y%m%d%H%M")
+        f = open("printer_buffer-"+nowstring+".txt", "wb")
 
-    print "printer listener listening"
-    while not line:
-	sleep(5)
-	line = ser.read(1)
+        while line:
+  	    print line
+	    f.write(line)
+	    line = ser.read(10)
 
-    print "printer active"
-    now = datetime.datetime.now()
-    nowstring = now.strftime("%Y%m%d%H%M")
-    f = open("printer_buffer-"+nowstring+".txt", "wb")
+        print "printer inactive for 15 seconds, closing up shop."
+        f.close()
+        line = ""
 
-    while line:
-	print line
-	f.write(line)
-	line = ser.read(10)
-
-    print "printer inactive for 15 seconds, closing up shop."
-    f.close()
-    line = ""
-
-ser.close()
+    ser.close()
 
